366c355891e6c84580b7fcdf06fe8f423a0cd9f9
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-07-02 17:21:57 -0300

    Capturando imagem do dispositivo em create-employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Camera } from "@ionic-native/camera/ngx";
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -17,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
     IonicModule.forRoot(),
     AppRoutingModule],
   providers: [
+    Camera,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/create-employee/create-employee.page.ts
+++ src/app/create-employee/create-employee.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DataService, Employee } from '../services/data.service';
 import { Router } from '@angular/router';
-import { ToastController } from '@ionic/angular';
+import { ToastController, AlertController } from '@ionic/angular';
+import { CameraOptions, Camera } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-create-employee',
@@ -12,11 +13,14 @@ import { ToastController } from '@ionic/angular';
 export class CreateEmployeePage implements OnInit {
   registrationFormGroup: FormGroup
   blankAvatar: string
+  avatar: string
 
   constructor(
     private _dataService: DataService,
     private _router: Router,
     private _toastController: ToastController,
+    private _alertController: AlertController,
+    private _camera: Camera,
     formBuider: FormBuilder
   ) {
     this.blankAvatar = this._dataService.getBlankAvatar()
@@ -30,8 +34,17 @@ export class CreateEmployeePage implements OnInit {
     })
   }
 
+  ngOnInit() {
+    this.registrationFormGroup.controls['avatarUrl'].valueChanges.subscribe(newAvatarUrl => {
+      this.avatar = newAvatarUrl
+  })
+  }
+
   register() {
-    const newEmployee: Employee = this.registrationFormGroup.value
+    const newEmployee: Employee = {
+      ...this.registrationFormGroup.value,
+      avatarUrl: this.avatar
+    }
     const wasCreated: boolean = this._dataService.createEmployee(newEmployee)
 
     const toast = this._toastController.create({
@@ -48,6 +61,55 @@ export class CreateEmployeePage implements OnInit {
       }, 1500)
   }
 
-  ngOnInit() {
+  async selectImageSource() {
+    const cameraOptions: CameraOptions = {
+      quality: 100,
+      // DATA_URL: a imagem será salva como uma string, com o método de codificação Base64
+      destinationType: this._camera.DestinationType.DATA_URL,
+      encodingType: this._camera.EncodingType.JPEG,
+      mediaType: this._camera.MediaType.PICTURE,
+      targetHeight: 350,
+      // a orientação da imagem ficará correta, independentimente a foto foi batida em modo paisagem ou não
+      correctOrientation: true
+    }
+
+    const alert = await this._alertController.create({
+      header: "Selecionar fonte",
+      message: "Selecione uma imagem a partir da câmera ou da sua galeria",
+      buttons: [
+        {
+          text: "Câmera",
+          handler: () => {
+            // indica que a fonte é a câmera
+            const sourceType = this._camera.PictureSourceType.CAMERA
+
+            this._camera.getPicture({...cameraOptions, sourceType})
+              .then(imageData => {
+                // caso a imagem seja selecionada, o imput de URL do Avatar será limpo
+                // É importante que limpe o imput avatarUrl primeiro, pois essa mudança disparará o evento de mudança, que irá atualizar a variável avatar para ""
+                this.registrationFormGroup.patchValue({ avatarUrl: "" })
+
+                this.avatar = `data:image/jpeg;base64,${imageData}`
+            })
+          }
+        },
+        {
+          text: "Galeria",
+          handler: () => {
+            // indica que a fonte é a galeria
+            const sourceType = this._camera.PictureSourceType.SAVEDPHOTOALBUM
+
+            this._camera.getPicture({...cameraOptions, sourceType})
+              .then(imageData => {
+                this.registrationFormGroup.patchValue({ avatarUrl: "" })
+                
+                this.avatar = `data:image/jpeg;base64,${imageData}`
+            })
+          }
+        }
+      ]
+    })
+
+    await alert.present()
   }
 }
