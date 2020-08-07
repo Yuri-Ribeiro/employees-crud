1a5d10b7eb952cab83f8d5821bb4208b4e569b61
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-08-07 08:07:12 -0300

    Adicionado validação de emails

--- src/app/create-employee/create-employee.page.ts
+++ src/app/create-employee/create-employee.page.ts
@@ -24,7 +24,7 @@ export class CreateEmployeePage implements OnInit {
     this.registrationFormGroup = formBuilder.group({
       avatarUrl:[""],
       name: [ , Validators.required],
-      email: [ , Validators.required],
+      email: [ , [Validators.required, Validators.email]],
       job: [],
       description: [],
     })
