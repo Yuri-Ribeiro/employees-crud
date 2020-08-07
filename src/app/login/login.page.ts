8ce9ac793783206da19a96d8dae5956f9102b625
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-08-07 07:53:12 -0300

    Corrigido local do serviço de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LoginCredentials, LoginService } from '../login.service';
+import { LoginCredentials, LoginService } from '../services/login.service';
 import { ToastController } from '@ionic/angular';
 
 @Component({
